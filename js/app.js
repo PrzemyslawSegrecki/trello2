1c2ce0ae15c9b07b4e7a8b4070d0cd9e1988403c
PrzemyslawSegrecki <przemyslaw@segrecki.pl>
2016-06-30 00:27:59 +0200

    Baza

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function TodoCtrl() {
+/*  this.todos = [
+    {text:'learn angular', done:true},
+    {text:'build an angular app', done:false}];
+
+  this.addTodo = function() {
+    this.todos.push({text:this.todoText, done:false});
+    this.todoText = '';
+  };
+
+this.remaining = function() {
+    var count = 0;
+    angular.forEach(this.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  this.archive = function() {
+    var oldTodos = this.todos;
+    this.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) this.todos.push(todo);
+    });
+  };*/
+}
